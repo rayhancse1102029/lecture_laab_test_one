a9ead19f8e843d00aedcc4650026d4a5f0532f1f
rayhancse15git <rayhancse15@gmail.com>
2020-11-16 09:10:15 +0600

    dfgfdg

--- LabTestOne/Models/Base.cs
+++ LabTestOne/Models/Base.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,9 @@ namespace LabTestOne.Models
 {
     public class Base
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
         public string createdBy { get; set; }
         public string updatedBy { get; set; }
         public DateTime? createdAt { get; set; }
--- LabTestOne/Models/IncomeTax.cs
+++ LabTestOne/Models/IncomeTax.cs
@@ -5,7 +5,11 @@ using System.Threading.Tasks;
 
 namespace LabTestOne.Models
 {
-    public class IncomeTax 
+    public class IncomeTax : Base
     {
+        public string nameOfAsse { get; set; }
+        public string nid { get; set; }
+        public string utin { get; set; }
+        public string tin { get; set; }
     }
 }
