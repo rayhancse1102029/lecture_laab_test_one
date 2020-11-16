2fa3a98167afce56b84f4f303a332c1f4af605de
rayhancse15git <rayhancse15@gmail.com>
2020-11-16 08:53:14 +0600

    sdfsdf

---
+++ LabTestOne/Models/Base.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LabTestOne.Models
+{
+    public class Base
+    {
+        public string createdBy { get; set; }
+        public string updatedBy { get; set; }
+        public DateTime? createdAt { get; set; }
+        public DateTime? updatedAt { get; set; }
+    }
+}
