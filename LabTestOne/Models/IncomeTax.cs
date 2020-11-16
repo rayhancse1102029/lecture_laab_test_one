52d1b0e0b9baf060ac73e9b9a0bcd78e7e8e2fd3
rayhancse15git <rayhancse15@gmail.com>
2020-11-16 09:24:06 +0600

    ghgjk

--- LabTestOne/Models/IncomeTax.cs
+++ LabTestOne/Models/IncomeTax.cs
@@ -11,5 +11,20 @@ namespace LabTestOne.Models
         public string nid { get; set; }
         public string utin { get; set; }
         public string tin { get; set; }
+        public string circel { get; set; }
+        public string taxZone { get; set; }
+        public string asseYear { get; set; }
+        public bool isResident { get; set; }
+        public bool familyStatus { get; set; }
+        public string nameOFEmployee { get; set; }
+        public string husbandORWife { get; set; }
+        public string father { get; set; }
+        public string mother { get; set; }
+        public string dateOFBirth { get; set; }
+        public string presentAddress { get; set; }
+        public string prmanentAddress { get; set; }
+        public string mobileOffice { get; set; }
+        public string residential { get; set; }
+        public string vatRegistrationNo { get; set; }
     }
 }
