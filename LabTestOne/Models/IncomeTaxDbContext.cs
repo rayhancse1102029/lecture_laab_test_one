40407f3ac5bec22e6d54d7f3a49556eea86323d7
rayhancse15git <rayhancse15@gmail.com>
2020-11-18 09:05:39 +0600

    dfdsfsdf

--- LabTestOne/Models/IncomeTaxDbContext.cs
+++ LabTestOne/Models/IncomeTaxDbContext.cs
@@ -7,6 +7,8 @@ using System.IO;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using LabTestOne.Data.Entity;
+using LabTestOne.Models;
 
 namespace LabTestOne.Data
 {
@@ -17,119 +19,12 @@ namespace LabTestOne.Data
         {
             this._httpContextAccessor = _httpContextAccessor;
         }
-
-        #region Settings Configs
-        public override int SaveChanges()
-        {
-            AddTimestamps();
-            return base.SaveChanges();
-        }
-
-        public async Task<int> SaveChangesAsync()
-        {
-            AddTimestamps();
-            return await base.SaveChangesAsync();
-        }
-
-        private void AddTimestamps()
-        {
-
-            var entities = ChangeTracker.Entries().Where(x => x.Entity is Base && (x.State == EntityState.Added || x.State == EntityState.Modified));
-
-            var currentUsername = !string.IsNullOrEmpty(_httpContextAccessor?.HttpContext?.User?.Identity?.Name)
-                ? _httpContextAccessor.HttpContext.User.Identity.Name
-                : "Anonymous";
-
-            foreach (var entity in entities)
-            {
-                if (entity.State == EntityState.Added)
-                {
-                    ((Base)entity.Entity).createdAt = DateTime.Now;
-                    ((Base)entity.Entity).createdBy = currentUsername;
-                }
-                else
-                {
-                    entity.Property("createdAt").IsModified = false;
-                    entity.Property("createdBy").IsModified = false;
-                    ((Base)entity.Entity).updatedAt = DateTime.Now;
-                    ((Base)entity.Entity).updatedBy = currentUsername;
-                }
-
-            }
-        }
-        #endregion
-
-        #region Attachment Comment
-        public DbSet<DocumentAttachment> DocumentAttachments { get; set; }
-        public DbSet<Comment> Comments { get; set; }
-        #endregion
-
-        #region Master Data
-        public DbSet<AddressType> AddressTypes { get; set; }
-        #endregion
-
-        #region User Manager
-        public DbSet<UserType> UserTypes { get; set; }
-        public DbSet<UserTypeGroup> UserTypeGroups { get; set; }
-        public DbSet<UserAccessPage> UserAccessPages { get; set; }
-        public DbSet<Navbar> Navbars { get; set; }
-        public DbSet<MenuBand> MenuBands { get; set; }
-        public DbQuery<NavbarViewModel> navbarViewModels { get; set; }
-        public DbQuery<UserAccessPageViewModel> userAccessPageViewModels { get; set; }
-        public DbQuery<UpdateAspnetUser> updateAspnetUsers { get; set; }
-        #endregion
-
-        #region Master Data
-        public DbSet<ERPModule> ERPModules { get; set; }
-        public DbSet<ERPModuleAssign> ERPModuleAssigns { get; set; }
-        public DbSet<ModuleAccessPage> ModuleAccessPages { get; set; }
-        public DbSet<DocumentPhotoAttachment> DocumentPhotoAttachments { get; set; }
-        public DbQuery<AspNetUsersViewModel> AspNetUsersViewModels { get; set; }
-        public DbSet<Address> Addresses { get; set; }        
-        public DbSet<Country> Countries { get; set; }
-        public DbSet<Department> Departments { get; set; }
-        public DbSet<Designation> Designations { get; set; }
-        public DbSet<District> Districts { get; set; }
-        public DbSet<Division> Divisions { get; set; }
-        public DbSet<Thana> Thanas { get; set; }
-        public DbSet<PostOffice> PostOffices { get; set; }
-        public DbSet<Resource> Resources { get; set; }       
-        public DbSet<AddressCategory> addressCategories { get; set; }
-
-        public DbSet<Company> Companies { get; set; }       
-        public DbSet<UserLogHistory> UserLogHistories { get; set; }
-        public DbSet<OrganizationType> OrganizationTypes { get; set; }      
-        public DbSet<Organization> Organizations { get; set; }      
-        public DbSet<StoreType> StoreTypes { get; set; }
-        public DbSet<Store> Stores { get; set; }
-        public DbSet<StoreAssign> StoreAssigns { get; set; }
-        public DbQuery<AspNetUsersListViewModel> aspNetUsersViews { get; set; }
-        public DbQuery<RoleType> RoleTypes { get; set; }
-        public DbQuery<UserBackup> UserBackups { get; set; }
-
-
-        #region
-
-        public DbSet<TaxYear> taxYears { get; set; }
-
-        #endregion
-
-        #endregion
-
+              
 
         #region Income Tax
 
-        public DbSet<TaxPayMaster> TaxPayMasters { get; set; }
-        public DbSet<IncomeAssesseeStatement> IncomeAssesseeStatements { get; set; }
-        public DbSet<IncomeScheduleDetailsOne> IncomeScheduleDetailsOnes { get; set; }
-        public DbSet<IncomeScheduleDetailsTwo> IncomeScheduleDetailsTwos { get; set; }
-        public DbSet<IncomeScheduleDetailsThree> IncomeScheduleDetailsThrees { get; set; }
-        public DbSet<FurnishedDocument> FurnishedDocuments { get; set; }
-        public DbSet<LiabilitiesAssetsStatement> LiabilitiesAssetsStatements { get; set; }
-        public DbSet<DirectorShareholding> DirectorShareholdings { get; set; }
-        public DbSet<IncomeTaxOrdinance> IncomeTaxOrdinances { get; set; }
-        public DbSet<ReturnForm> ReturnForms { get; set; }
-
+        public DbSet<IncomeTax> incomeTaxes { get; set; }
+        
         #endregion
     }
 }
--- LabTestOne/Startup.cs
+++ LabTestOne/Startup.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LabTestOne.Data;
+using LabTestOne.Data.Entity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
 
 namespace LabTestOne
 {
@@ -32,7 +38,85 @@ namespace LabTestOne
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            #region PDF
+            //services.AddSingleton(typeof(IConverter), new SynchronizedConverter(new PdfTools()));
+            #endregion
+
+            
+            #region
+
+            //services.AddScoped<ITaxPayService, TaxPayService>();
+
+            #endregion
+
+
+            #region Database Settings
+            services.AddDbContext<IncomeTaxDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<IncomeTaxDbContext>();
+            #endregion
+
+            #region Auth Related Settings
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings.
+                options.User.RequireUniqueEmail = true;
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 4;
+                options.Password.RequiredUniqueChars = 1;
+
+                // Lockout settings.
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // User settings.
+                options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = false;
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                // Cookie settings
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromHours(1);
+
+                options.LoginPath = "/Auth/Account/Login";
+                //options.LoginPath = "/Ecommerce/EcommerceWebSite/Index";
+                //options.AccessDeniedPath = "/Auth/Account/AccessDenied";
+                //options.SlidingExpiration = true;
+            });
+            #endregion
+
+            #region Areas Config
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.AreaViewLocationFormats.Clear();
+                options.AreaViewLocationFormats.Add("/areas/{2}/Views/{1}/{0}.cshtml");
+                options.AreaViewLocationFormats.Add("/areas/{2}/Views/Shared/{0}.cshtml");
+                options.AreaViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
+            });
+            #endregion
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddJsonOptions(options => {
+                    var resolver = options.SerializerSettings.ContractResolver;
+                    if (resolver != null)
+                        (resolver as DefaultContractResolver).NamingStrategy = null;
+                });
+
+            services.AddSession(options =>
+            {
+                options.Cookie.Name = ".AdventureWorks.Session";
+                options.IdleTimeout = TimeSpan.FromHours(24);
+                options.Cookie.IsEssential = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,22 +125,40 @@ namespace LabTestOne
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
             }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
+            app.UseSession();
+            app.UseStaticFiles();
+            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
-                    name: "default",
+                      name: "areas",
+                      template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                    name: "controllers",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                //routes.MapRoute(
+                //    name: "controller",
+                //    template: "{controller=exists}/{action=Index}/{id?}");
+
+                routes.MapAreaRoute(
+                    name: "default",
+                    areaName: "Auth",
+                    template: "{controller=Account}/{action=Login}/{id?}");
             });
         }
     }
